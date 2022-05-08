09e1b9f10c7b6a45958b2eedb5052978882f7908
skiptothegoodbit <harrybaouab@gmail.com>
2022-05-08 19:52:03 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -112,7 +112,7 @@ export default {
         }
         </script>
 
-   * 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   * 	
         	<script>
         	
         	    var xhr = new XMLHttpRequest();
