588f7d879d6a31aab478b38b7791a191b8cad08e
skiptothegoodbit <harrybaouab@gmail.com>
2022-05-08 17:39:45 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,3 +9,50 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+$("div").click(function(e){
+   var url = "https://api.spotify.com/v1/me/player/next";
+   var xhr = new XMLHttpRequest();
+   var url1 = "https://api.spotify.com/v1/me/player/previous";
+   var pWidth = $(this).innerWidth(); //use .outerWidth() if you want borders
+   var pOffset = $(this).offset(); 
+   var x = e.pageX - pOffset.left;
+    if(pWidth/2 > x){
+      $(this).text('left')
+      xhr.open("POST", url);
+      }
+    else{
+      $(this).text('Right')
+      xhr.open("POST", url1);
+      }
+          
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Authorization", "Bearer BQC8Z3I7T1dxTFY7l7jpbKI2bIcxzJmzY2Fr1B5XdzEYSmLwJFEqgwe8DQQo-YdCk_mytDneKfo4vR1RT9tjmctQ4_LvT6tOZOefMr7EVPwbe8jjyhnBm9TtGo_0csW6jBydbI_7hji9bsOE3gpuO4iyOQL1dTQhTnN6AEY");
+xhr.setRequestHeader("Content-Length", "0");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+   
+
+xhr.send()
+;
+});
+
+
+
+
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Authorization", "Bearer BQC8Z3I7T1dxTFY7l7jpbKI2bIcxzJmzY2Fr1B5XdzEYSmLwJFEqgwe8DQQo-YdCk_mytDneKfo4vR1RT9tjmctQ4_LvT6tOZOefMr7EVPwbe8jjyhnBm9TtGo_0csW6jBydbI_7hji9bsOE3gpuO4iyOQL1dTQhTnN6AEY");
+xhr.setRequestHeader("Content-Length", "0");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      }
+}
